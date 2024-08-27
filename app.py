b71550d0330627278315f0edc0ed70e9d4da38af
Srujan-rai <raisrujan@gmail.com>
2024-08-27 07:50:24 +0530

    files added

--- app.py
+++ app.py
@@ -3,9 +3,24 @@ import os
 from werkzeug.utils import secure_filename
 from PIL import Image
 import pytesseract
+import mimetypes
+from dotenv import load_dotenv
+import google.generativeai as genai
 
 
 app=Flask(__name__)
+
+API_KEY =os.getenv("API_KEY")
+
+genai.configure(api_key=API_KEY)
+generation_config = {
+    "temperature": 1,
+    "top_p": 0.95,
+    "top_k": 64,
+    "max_output_tokens": 8192,
+    "response_mime_type": "text/plain",
+}
+
 
 saved_file_path='uploads'
 
@@ -41,17 +56,30 @@ def process():
         return render_template('home.html',message=None)
     
     else:
-        print("srujan")
         file=request.files['file']
         file_path=os.path.join(saved_file_path,secure_filename(file.filename))
         file.save(file_path)
+        mime_type,_=mimetypes.guess_type(file_path)
+        if mime_type in ['image/png', 'image/jpeg']:
+            message=process_image(file_path)
+
         
-        message=process_image(file_path)
+        elif mime_type == 'application/pdf':
+            message="uploaded is pdf"
+
+        
         
         os.remove(file_path)
         
         return jsonify({"message":message})
-    
+  
+@app.route('/chat',methods=['POST','GET'])
+def chat():
+    if request.method=='POST':
+        query=request.form['query']
+        print(query)
+        
+    return render_template("home.html",message=query)
         
         
         
